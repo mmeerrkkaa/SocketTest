9108d05daedaba0464e27acee4bcdbb550df4a53
mmeerrkkaa <55891034+mmeerrkkaa@users.noreply.github.com>
2021-06-05 19:16:23 +0300

    Update serverNEW.py

--- serverNEW.py
+++ serverNEW.py
@@ -1,4 +1,5 @@
-import asyncio, socket
+import asyncio
+import socket
 import ast
 import math
 import sqlite3
@@ -13,72 +14,76 @@ cursor.execute(f"CREATE TABLE users(name STRING, games INTEGER, atmps INTEGER, l
 conn.commit()
 
 
-client = {} # тут я буду хранить подключившивхся игроков (класс хранить)
-
-class User:  # тут мы выполняем все действия связанные с аккаунтами (в будущем по крайне мере)
-	def __init__(self, name):
-		self.name = name
+client = {}  # тут я буду хранить подключившивхся игроков (класс хранить)
 
 
-	def Connection(self):
-		print(f"[{self.name}] Подключился")
-		print(client[self.name])
-		return User.CheckReg(self) # проверка на существование
+# тут мы выполняем все действия связанные с аккаунтами (в будущем по крайне мере)
+class User:
+    def __init__(self, name):
+        self.name = name
 
-	def CheckReg(self):
-		if cursor.execute(f'SELECT * FROM users where name="{self.name}"').fetchone() == None: # Если аккаунт не создан, то создаём
-			sqliteAdd = json.dumps("[]")
-			cursor.execute(f"INSERT INTO users VALUES ('{self.name}', '0', '0', {sqliteAdd}, '100')")#вводит все данные об участнике в БД
-			conn.commit()
-			print(f"{self.name} - регистрирует аккаунт")
-			return "Вы зарегистрировали аккаунт"
+    def Connection(self):
+        print(f"[{self.name}] Подключился")
+        print(client[self.name])
+        return User.CheckReg(self)  # проверка на существование пользователя
 
-		else: # Если создан, то загружаем данные из базы данных
-			self.games = cursor.execute(f'SELECT games FROM users where name="{self.name}"').fetchone()[0]
-			self.atmps = cursor.execute(f'SELECT atmps FROM users where name="{self.name}"').fetchone()[0]
-			self.log = ast.literal_eval(cursor.execute(f'SELECT log FROM users where name="{self.name}"').fetchone()[0])
-			self.RandomNumber = cursor.execute(f'SELECT RandomNumber FROM users where name="{self.name}"').fetchone()[0]
-			print(f"[{self.name}] Аккаунт загружен")
-			return "Аккаунт загружен"
+    def CheckReg(self):
+        # Если аккаунт не создан, то создаём
+        if cursor.execute(f'SELECT * FROM users where name="{self.name}"').fetchone() == None:
+            sqliteAdd = json.dumps("[]")
+            # вводит все данные об участнике в БД
+            cursor.execute(f"INSERT INTO users VALUES ('{self.name}', '0', '0', {sqliteAdd}, '100')")
+            conn.commit()
+            print(f"{self.name} - регистрирует аккаунт")
+            return "Вы зарегистрировали аккаунт"
 
-	def GetInfo(self):
-		print(1)
-		return self.name
+        else:  # Если создан, то загружаем данные из базы данных
+            self.games = cursor.execute(f'SELECT games FROM users where name="{self.name}"').fetchone()[0]
+            self.atmps = cursor.execute(f'SELECT atmps FROM users where name="{self.name}"').fetchone()[0]
+            self.log = ast.literal_eval(cursor.execute(f'SELECT log FROM users where name="{self.name}"').fetchone()[0])
+            self.RandomNumber = cursor.execute(f'SELECT RandomNumber FROM users where name="{self.name}"').fetchone()[0]
+            print(f"[{self.name}] Аккаунт загружен")
+            return "Ваш акаунт загружен"
+
+    def GetInfo(self):
+        print(1)
+        return self.name
 
 
 async def handle_client(reader, writer):
 
+    while 1:
+        # Получаем имя пользователя. Если следующая команда не поступит, значит пользователль вышел
+        user = (await reader.read(1024)).decode('utf8')[:-1]
+        data = (await reader.read(1024)).decode('utf8')
 
-	while 1:
-		user = (await reader.read(1024)).decode('utf8') # Получаем имя пользователя. Если следующая команда не поступит, значит пользователль вышел
-		data = (await reader.read(1024)).decode('utf8')
+        try:  # Если можем преобразовать текст, значит всё ок
+            data = ast.literal_eval(data)
+        except:  # Тут мы можем получить только пустую строку, если была получена пустая строка, значит пользователь вышел
+            print("exc")
+            if user in client:
+                print(client)
+                client.pop(user)
+                print(client)
+                break
+            else:
+                break
+        print(data, user)
 
-		try: # Если можем преобразовать текст, значит всё ок
-			data = ast.literal_eval(data)
-		except: # Тут мы можем получить только пустую строку, если была получена пустая строка, значит пользователь вышел
-			print("exc")
-			if user[:-1] in client:
-				print(client)
-				client.pop(user[:-1])
-				print(client)
-				break
-			else:
-				break
-		print(data, user)
-
-
-		if data[0] == "Connection": # тут я буду принимать команды и обрабатывать. Шифрования нет, т.к тренировочный проект
-			Users = User(data[1])
-			client[data[1]] = Users
-			response = client[data[1]].Connection()
-
-        #elif data[0] in dir(User):  # тут не работает.
-        #	response = f'{client[data[1]].{data[0]}()}\n' # Я хотел проверять, существует ли функция в классе и если существует, то вызвать её
+        # тут я буду принимать команды и обрабатывать. Шифрования нет, т.к тренировочный проект
+        if data[0] == "Connection":
+            Users = User(user)
+            client[user] = Users
+            response = client[user].Connection()
 
 
-		writer.write(response.encode('utf8'))
-		await writer.drain()
-	writer.close()
+    # elif data[0] in dir(User):  # тут не работает.
+    #	response = f'{client[user].{data[0]}()}\n' # Я хотел проверять, существует ли функция в классе и если существует, то вызвать её
+
+        writer.write(response.encode('utf8'))
+        await writer.drain()
+    writer.close()
+
 
 async def run_server():
     server = await asyncio.start_server(handle_client, 'localhost', 15557)
