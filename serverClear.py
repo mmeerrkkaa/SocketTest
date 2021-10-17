a12f5b7654eea2cce168084db28386705f8582fd
mmeerrkkaa <55891034+mmeerrkkaa@users.noreply.github.com>
2021-10-17 11:01:23 +0300

    Create serverClear.py

---
+++ serverClear.py
@@ -0,0 +1,23 @@
+import jpysocket
+import socket
+import asyncio
+
+async def handle_client(reader, writer):
+	#print(reader, writer)
+	while 1:
+		data = (await reader.read(1024)).decode('utf8')
+
+		print(data)
+		response = data
+		writer.write(response.encode('utf8'))
+		await writer.drain()
+
+	writer.close()
+
+
+async def run_server():
+	server = await asyncio.start_server(handle_client, '127.0.0.1', 15557)
+	async with server:
+		await server.serve_forever()
+
+asyncio.run(run_server())
